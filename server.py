05510d5bc6b3b3dcc3544aa8dcd5a6c6c7298aac
brkzin789 <brkzingit26@gmail.com>
2026-04-04 11:37:24 -0300

    server.py

--- server.py
+++ server.py
@@ -210,5 +210,8 @@ def delete_reply(id):
     return redirect(f"/thread/{row['thread_id']}")
 
 # ================== EXECUTAR ==================
+
+    import os
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
